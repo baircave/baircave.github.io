87524410033a5597efd4e3995b07147e85f728fb
Nick Newhouse <nicknewhouse@Nicks-MacBook-Pro.local>
2024-03-19 13:40:44 -0400

    fix some bugs, add songs, add socials, optimize for mobile

--- js/site.js
+++ js/site.js
@@ -4,19 +4,31 @@ const trackTitles = [
 	"Nick Newhouse - Overfalls",
 	"Nick Newhouse - Let Me Cry",
 	"Walter - Call of the Wild",
-	"Walter - The Inside"
+	"Walter - The Inside",
+	"Baircave & Catmosphere - The Siren Song [Radio Edit]",
+	"Baircave & arthur x medic - Alice",
+	"Baircave & Voia - Folds",
+	"Jay Sean - Down [NN Bootleg]"
 ];
 const trackFilenames = [
 	'Overfalls.mp3', 
 	'Let_Me_Cry.mp3',
 	'cotw.mp3',
-	'the_inside.mp3'
+	'the_inside.mp3',
+	'the_siren_song.mp3',
+	'Alice.mp3',
+	'Folds.mp3',
+	'down.mp3'
 ];
 const credits = [
-	'written, produced, mixed, and mastered by me', 
-	'written, produced, mixed, and mastered by me',
-	'produced, mixed, and mastered by me',
-	'co-written, produced, mixed, and mastered by me'
+	'written, produced, mixed, & mastered by me', 
+	'written, produced, mixed, & mastered by me',
+	'produced, mixed, & mastered by me',
+	'produced, mixed, & mastered by me; written by me & Walter',
+	'written & mastered by me; produced & mixed by me & Catmosphere',
+	'mixed & mastered by me; produced & written by me & arthur x medic',
+	'mixed & mastered by me; produced & written by me & voia',
+	'remixed by me (w/o permission, so SHH!!!)'
 ];
 const audio = document.getElementById('audio-player');
 const elapsedTime = document.getElementById('elapsed-time');
@@ -27,8 +39,10 @@ const backgroundNN = document.getElementById('nn-video');
 const headshot = document.getElementById('headshot');
 const glitch = document.getElementById('glitch');
 
+var videosLoaded = 0;
 var isPlaying = 0;
 var isHeadshot = 0;
+var isCredits = 0;
 var currentTrack = 0;
 var playPauseButton = document.getElementById("play-pause");
 
@@ -77,14 +91,6 @@ function rewind() {
 	}
 }
 
-function updateTrackInfo() {
-	trackTitle.style.opacity = 0;
-	setTimeout(() => {
-		trackTitle.innerHTML = trackTitles[currentTrack];
-		trackTitle.style.opacity = 1;
-	}, 700)
-}
-
 function nextTrack() {
 	audio.pause();
 	audio.currentTime = 0;
@@ -95,7 +101,7 @@ function nextTrack() {
 		currentTrack = 0;
 	}
 
-	updateTrackInfo();
+	rotateCreditsWithTitle(1);
 	audio.src = `audio/${trackFilenames[currentTrack]}`;
 	play();
 }
@@ -110,19 +116,21 @@ function previousTrack() {
 		currentTrack -= 1;
 	}
 
-	updateTrackInfo();
+	rotateCreditsWithTitle(1);
 	audio.src = `audio/${trackFilenames[currentTrack]}`;
 	play();
 }
 
-function rotateCreditsWithTitle() {
+function rotateCreditsWithTitle(setTitle) {
 	trackTitle.style.opacity = 0;
 
 	setTimeout(() => {
-		if (trackTitle.innerHTML == trackTitles[currentTrack]) {
-			trackTitle.innerHTML = credits[currentTrack];
-		} else {
+		if (isCredits || setTitle) {
 			trackTitle.innerHTML = trackTitles[currentTrack];
+			isCredits = 0;
+		} else {
+			trackTitle.innerHTML = credits[currentTrack];
+			isCredits = 1;
 		}
 
 		trackTitle.style.opacity = 1;
@@ -132,23 +140,15 @@ function rotateCreditsWithTitle() {
 function switchVideos() {
 	if (isHeadshot) {
 		headshot.style.zIndex = -2;
-		// headshot.style.visibility = "hidden";
-		// glitch.style.visibility = "visible";
 		setTimeout(() => {
 			isHeadshot = 0;
 			backgroundNN.style.zIndex = 0;
-			// glitch.style.visibility = "hidden";
-			// backgroundNN.style.visibility = "visible";
 		}, 500);
 	} else {
-		// backgroundNN.style.visibility = "hidden";
-		// glitch.style.visibility = "visible";
 		backgroundNN.style.zIndex = -2
 		setTimeout(() => {
 			isHeadshot = 1;
 			headshot.style.zIndex = 0;
-			// glitch.style.visibility = "hidden";
-			// headshot.style.visibility = "visible";
 		}, 500);
 	}
 }
@@ -164,6 +164,19 @@ function fixVideoSources() {
 	}
 }
 
+function syncVideos() {
+	if (videosLoaded < 3) {
+		setTimeout(() => {
+			syncVideos();
+			
+		}, 200);
+		return;
+	}
+
+	backgroundNN.play();
+	glitch.play();
+	headshot.play();
+}
 
 // spacebar play pause
 document.body.onkeyup = function(e) {
@@ -186,6 +199,9 @@ document.body.onkeydown = function(e) {
 }
 
 trackTitle.innerHTML = trackTitles[currentTrack];
+backgroundNN.onloadeddata = () => videosLoaded += 1;
+headshot.onloadeddata = () => videosLoaded += 1;
+glitch.onloadeddata = () => videosLoaded += 1;
 audio.onended = () => nextTrack();
 audio.onloadeddata = () => {
 	setElapsedTime();
@@ -197,7 +213,11 @@ audio.load();
 setInterval(() => {
 	rotateCreditsWithTitle();
 }, 10000);
+setInterval(() => {
+	switchVideos();
+}, 60000);
 fixVideoSources();
+syncVideos();
 
 navigator.mediaSession.setActionHandler('play', () => play());
 navigator.mediaSession.setActionHandler('pause', () => pause());
@@ -206,73 +226,71 @@ navigator.mediaSession.setActionHandler('seekforward', () => fastForward());
 navigator.mediaSession.setActionHandler('previoustrack', () => previousTrack());
 navigator.mediaSession.setActionHandler('nexttrack', () => nextTrack());
 
-// canvas stuff!
+// // canvas stuff!
 
-// wait for the content of the window element
-// to load, then performs the operations.
-// This is considered best practice.
-window.addEventListener('load', ()=>{
-	resize(); // Resizes the canvas once the window loads
-	document.addEventListener('mousemove', sketch);
-	window.addEventListener('resize', resize);
-});
+// // wait for the content of the window element
+// // to load, then performs the operations.
+// // This is considered best practice.
+// window.addEventListener('load', ()=>{
+// 	resize(); // Resizes the canvas once the window loads
+// 	document.addEventListener('mousemove', sketch);
+// 	window.addEventListener('resize', resize);
+// });
 
-const canvas = document.querySelector('#canvas');
+// const canvas = document.querySelector('#canvas');
 
-// Context for the canvas for 2 dimensional operations
-const ctx = canvas.getContext('2d');
+// // Context for the canvas for 2 dimensional operations
+// const ctx = canvas.getContext('2d');
 
-// Resizes the canvas to the available size of the window.
-function resize() {
-	ctx.canvas.width = window.innerWidth;
-	ctx.canvas.height = window.innerHeight;
-}
+// // Resizes the canvas to the available size of the window.
+// function resize() {
+// 	ctx.canvas.width = window.innerWidth;
+// 	ctx.canvas.height = window.innerHeight;
+// }
 
-// Stores the initial position of the cursor
-let coord = {x:500 , y:500};
+// // Stores the initial position of the cursor
+// let coord = {x:500 , y:500};
 
-let mouseUpdateCounter = 1; 
+// let mouseUpdateCounter = 1; 
 
-// Updates the coordianates of the cursor when
-// an event e is triggered to the coordinates where
-// the said event is triggered.
-function getPosition(event){
-	if (mouseUpdateCounter % 50 == 0) {
-		mouseUpdateCounter = 1
+// // Updates the coordianates of the cursor when
+// // an event e is triggered to the coordinates where
+// // the said event is triggered.
+// function getPosition(event){
+// 	if (mouseUpdateCounter % 50 == 0) {
+// 		mouseUpdateCounter = 1
 
-		coord.x = event.clientX - canvas.offsetLeft;
-		coord.y = event.clientY - canvas.offsetTop;
-	} else {
-		mouseUpdateCounter += 1;
-	}
-}
+// 		coord.x = event.clientX - canvas.offsetLeft;
+// 		coord.y = event.clientY - canvas.offsetTop;
+// 	} else {
+// 		mouseUpdateCounter += 1;
+// 	}
+// }
 
-// The following functions toggle the flag to start
-// and stop drawing
-function startPainting(event){
-	getPosition(event);
-}
+// function startPainting(event){
+// 	getPosition(event);
+// }
 
-function sketch(event){
-	ctx.beginPath();
+// function sketch(event){
+// 	ctx.beginPath();
 
-	ctx.lineWidth = 2;
+// 	ctx.lineWidth = 2;
 
-	ctx.strokeStyle = 'rgba(40,25,100, 1)';
+// 	ctx.strokeStyle = 'rgba(40,25,100, 1)';
 	
-	// The cursor to start drawing
-	// moves to this coordinate
-	ctx.moveTo(coord.x, coord.y);
+// 	// The cursor to start drawing
+// 	// moves to this coordinate
+// 	ctx.moveTo(coord.x, coord.y);
 
-	// The position of the cursor
-	// gets updated as we move the
-	// mouse around.
-	getPosition(event);
+// 	// The position of the cursor
+// 	// gets updated as we move the
+// 	// mouse around.
+// 	getPosition(event);
 
-	// A line is traced from start
-	// coordinate to this coordinate
-	ctx.lineTo(coord.x , coord.y);
+// 	// A line is traced from start
+// 	// coordinate to this coordinate
+// 	ctx.lineTo(coord.x , coord.y);
 
-	// Draws the line.
-	ctx.stroke();
-}
+// 	// Draws the line.
+// 	ctx.stroke();
+// }
